6a6d9c618836342cd6e664af6399a48188b85ea1
ZIM555 <zim555@mail.ru>
2014-08-10 19:45:24 +0300

    Update vendor

---
+++ logan-common-vendor-blobs.mk
@@ -0,0 +1,3 @@
+# PRODUCT_COPY_FILES += \
+#	vendor/samsung/logan/proprietary/vendor/lib/hw/lights.hawaii.so:system/vendor/lib/hw/lights.hawaii.so \
+#   vendor/samsung/logan/proprietary/vendor/lib/hw/power.hawaii.so:system/vendor/lib/hw/power.hawaii.so
